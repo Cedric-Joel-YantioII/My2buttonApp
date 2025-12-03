1a8a1f4bda5ab49dfbb65172bddac46889b847f0
Cedric-Joel-YantioII <Cedric.yantio@ndsu.edu>
2025-12-02 21:34:28 -0600

    Commiting
    Permission feature

--- app/src/main/java/com/assignment2/my2buttonapp/MainActivity.java
+++ app/src/main/java/com/assignment2/my2buttonapp/MainActivity.java
@@ -1,17 +1,31 @@
 package com.assignment2.my2buttonapp;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String CUSTOM_PERMISSION = "My2ButtonApp.MSE412";
+
+    private ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this, CUSTOM_PERMISSION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissionLauncher.launch(CUSTOM_PERMISSION);
+        }
 
         Button explicitButton = findViewById(R.id.button);
         Button implicitButton = findViewById(R.id.button4);
